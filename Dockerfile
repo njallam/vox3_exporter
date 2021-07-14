35456ee10a0502d13c4adc085848ef2c28c064ca
Nicholas Allam <njallam@outlook.com>
2021-07-14 17:27:14 +0100

    Add Dockerfile and check presence of environment variables

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM golang:alpine as builder
+RUN apk update && apk add --no-cache git
+WORKDIR $GOPATH/src/vox3_exporter
+COPY . .
+RUN go get -d -v
+RUN CGO_ENABLED=0 GOOS=linux go build -a -ldflags="-s -w" -o /go/bin/vox3_exporter
+
+FROM scratch
+COPY --from=builder /go/bin/vox3_exporter /go/bin/vox3_exporter
+EXPOSE 9917
+ENTRYPOINT ["/go/bin/vox3_exporter"]
--- main.go
+++ main.go
@@ -9,10 +9,26 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func main() {
-	prometheus.MustRegister(newVox3Collector(os.Getenv("VOX3_IP"), os.Getenv("VOX3_PASSWORD")))
+const metricsPath = "/metrics"
 
-	http.Handle("/metrics", promhttp.Handler())
-	log.Println("Beginning to serve on port :9906")
-	log.Fatal(http.ListenAndServe(":9906", nil))
+func main() {
+	log.Println("Starting Vox3 Exporter")
+
+	ip := os.Getenv("VOX3_IP")
+	if len(ip) == 0 {
+		ip = "192.168.1.1"
+	}
+	password := os.Getenv("VOX3_PASSWORD")
+	if len(password) == 0 {
+		log.Fatalf("Missing environment variable VOX3_PASSWORD")
+	}
+
+	prometheus.MustRegister(newVox3Collector(ip, os.Getenv("VOX3_PASSWORD")))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html><head><title>Vox3 Exporter</title></head><body><h1>Vox3 exporter</h1><p><a href="metrics">Metrics</a></p></body></html>`))
+	})
+	http.Handle(metricsPath, promhttp.Handler())
+	log.Println("Beginning to serve on port :9917")
+	log.Fatal(http.ListenAndServe(":9917", nil))
 }
